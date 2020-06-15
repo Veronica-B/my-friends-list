4d0d806726b0c3e24c3cd4d55ee53f7bb61e960a
Veronica Barcia <veronicabarcia4@gmail.com>
2020-06-15 15:07:59 -0400

    first push

---
+++ Desktop/portfolio/friendslist/src/components/FriendCard/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.css'
+
+const FriendCard= (props) =>{
+    return(
+        <div className="card">
+            <div className="img-container">
+                <img src={props.image} alt={props.name}/>
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name: {props.name}</strong>
+                    </li>
+
+                    <li>
+                        <strong>Occupation: {props.occupation}</strong>
+                    </li>
+
+                    <li>
+                        <strong>Location: {props.location}</strong>
+                    </li>
+                </ul>
+            </div>
+        <span onClick={()=>props.removeFriend(props.id)} className="remove">X</span>
+        </div>
+    )
+
+}
+
+export default FriendCard;
